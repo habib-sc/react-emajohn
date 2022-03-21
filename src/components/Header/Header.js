80baefa06de24a0fb3f939c3ce8b08b9327a1a2a
habib-sc <habib97.cs@gmail.com>
2022-03-22 01:08:00 +0600

    Added some utilities and images files, added menubar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='nav-container'>
+            <nav>
+                <div className="logo">
+                    <img src={logo} alt="Logo" />
+                </div>
+                <div className="menus">
+                    <a href="/">Home</a>
+                    <a href="/products">Products</a>
+                    <a href="/order">Order</a>
+                    <a href="/inventory">Inventory</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
